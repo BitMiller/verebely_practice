e9f32ea71efcdfa0985b29cd8fea13d6bd4294c4
Peter Gall <gp.bitmiller@gmail.com>
2025-03-01 09:46:20 +0100

    Initial commit

---
+++ at250301-6 - AV02/main.py
@@ -0,0 +1,22 @@
+
+"""
+Termux:
+cd /storage/emulated/0/BitMiller/Pradhana/Dropbox/Prog/Python_practice_Verebely
+git init
+git remote add origin https://github.com/BitMiller/Python_practice_Verebely.git
+
+setted up ssh (reg.php:github) for not having to set token all the times
+
+git remote set-url origin git@github.com:BitMiller/https://github.com/BitMiller/Python_practice_Verebely.git
+
+git add .
+git commit -m "Initial commit"
+git push -u origin main
+
+
+
+"""
+
+
+
+
